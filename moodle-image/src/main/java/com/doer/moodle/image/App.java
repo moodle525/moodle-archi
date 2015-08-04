b375582aaccc169b92015eb8a9299a557ef7ad57
lixiongcheng <709597311@qq.com>
2015-08-04 18:07:58 +0800

    moodle-image

---
+++ moodle-image/src/main/java/com/doer/moodle/image/App.java
@@ -0,0 +1,13 @@
+package com.doer.moodle.image;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
