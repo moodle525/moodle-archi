980a7322b4786e4b107dfdf5cc5332d64ce9ef33
lixiongcheng <709597311@qq.com>
2016-03-17 23:17:27 +0800

    阿达

---
+++ moodle-mybatis/src/main/java/com/doer/moodle/mybatis/dao/intf/IPmsUserDao.java
@@ -0,0 +1,25 @@
+package com.doer.moodle.mybatis.dao.intf;
+
+import com.doer.moodle.mybatis.dao.IBaseDao;
+import com.doer.moodle.mybatis.dao.entity.PmsUser;
+
+
+/**
+ * 
+ * @描述: 用户表数据访问层接口.
+ * @作者: WuShuicheng .
+ * @创建时间: 2013-7-22,下午5:51:47 .
+ * @版本: 1.0 .
+ */
+public interface IPmsUserDao extends IBaseDao<PmsUser> {
+
+	/**
+	 * 根据用户登录名获取用户信息.
+	 * 
+	 * @param loginName
+	 *            .
+	 * @return user .
+	 */
+	PmsUser findByUserNo(String userNo);
+
+}
