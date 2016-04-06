3b94f7b8979fcb2f42aa3d81038a98616a2ecc8a
lixiongcheng <709597311@qq.com>
2016-04-06 22:47:52 +0800

    sss

--- moodle-cache/src/main/java/com/doer/moodle/cache/RedisClient.java
+++ moodle-cache/src/main/java/com/doer/moodle/cache/RedisClient.java
@@ -1,10 +1,14 @@
 package com.doer.moodle.cache;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.doer.moodle.cache.core.RedisClientTemplate;
 
+@Repository
 public class RedisClient {
+	//private static final Logger log = Logger.getLogger(RedisClient.class);
+	
 	@Autowired
 	private RedisClientTemplate clientTemplate;
 
@@ -13,23 +17,86 @@ public class RedisClient {
 	 *********************************************************/
 	/**
 	 * redis key所对应值得类型
+	 * 
 	 * @param key
 	 * @return
 	 */
 	public String type(String key) {
 		return clientTemplate.type(key);
 	}
-	
+
 	/**
 	 * 删除某个key
+	 * 
 	 * @param key
 	 * @return
 	 */
-	public Long del(String key){
+	public long del(String key) {
 		return clientTemplate.del(key);
 	}
 	
-	public String get(String key){
+	/**
+	 * key是否存在
+	 * @param key
+	 * @return
+	 */
+	public boolean exists(String key){
+		return clientTemplate.exists(key);
+	}
+
+	/**
+	 * 查询key的生命周期</br>
+	 * 不存在的key,返回-2,已过期的key/不过期的key,都返回-1</br>
+	 * 对于设置过过期时间的key返回有效期的秒数
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public long ttl(String key) {
+		return clientTemplate.ttl(key);
+	}
+
+	/**
+	 * key多少秒后过期
+	 * 
+	 * @param key
+	 * @param seconds
+	 * @return
+	 */
+	public long expire(String key, int seconds) {
+		return clientTemplate.expire(key, seconds);
+	}
+
+	public long expireAt(String key, long seconds) {
+		return clientTemplate.expireAt(key, seconds);
+	}
+
+	/***************************************************************************
+	 * redis 对字符串操作
+	 ***********************************************************************/
+	/**
+	 * 存储字符串，失败抛异常
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void set(String key, String value) {
+		clientTemplate.set(key, value);
+	}
+	
+	/**
+	 * 更新key，保持key的有效期
+	 * @param key
+	 * @param value
+	 */
+	public void update(String key,String value){
+		if(!exists(key)){
+			throw new RuntimeException("no key "+key+" exists,please cheak it");
+		}
+		clientTemplate.setex(key, (int)ttl(key), value);
+	}
+
+	public String get(String key) {
 		return clientTemplate.get(key);
 	}
 }
--- moodle-test/src/test/java/com/doer/moodle/test/redis/RedisTest.java
+++ moodle-test/src/test/java/com/doer/moodle/test/redis/RedisTest.java
@@ -6,17 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.doer.moodle.cache.core.RedisClientTemplate;
+import com.doer.moodle.cache.RedisClient;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:spring/beans.xml" })
 public class RedisTest {
 	@Autowired
-	private RedisClientTemplate redis;
+	private RedisClient redis;
 
 	@Test
 	public void test01(){
 		redis.set("zhangsan", "是");
 		System.out.println(redis.get("zhangsan"));
 	}
+	
+	@Test
+	public void ttl(){
+		System.out.println(redis.expire("zhangsan", 50));
+		System.out.println(redis.ttl("zhangsan"));
+	}
+	
+	@Test
+	public void update(){
+		redis.update("zhangsana", "hee");
+		System.out.println(redis.ttl("zhangsan"));
+	}
+	
 }
