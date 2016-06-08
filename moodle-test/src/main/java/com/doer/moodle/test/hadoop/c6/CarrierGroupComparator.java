c15095350da73558432abaeb8114c6787b0122dd
lixiongcheng <709597311@qq.com>
2016-06-08 22:42:16 +0800

    。。。。

---
+++ moodle-test/src/main/java/com/doer/moodle/test/hadoop/c6/CarrierGroupComparator.java
@@ -0,0 +1,20 @@
+package com.doer.moodle.test.hadoop.c6;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class CarrierGroupComparator extends WritableComparator {
+    
+    public CarrierGroupComparator() {
+
+        super(CarrierKey.class, true);
+
+    }
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        CarrierKey first = (CarrierKey) a;
+        CarrierKey second = (CarrierKey) b;
+        return first.code.compareTo(second.code);
+    }
+
+}
